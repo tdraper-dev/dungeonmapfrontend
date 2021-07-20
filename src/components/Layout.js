3eff8388e12959cdff813e6b4e19495e3cca4b25
Travis Draper <tdanieldraper@gmail.com>
2021-07-19 23:18:29 -0400

    .

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,10 +3,30 @@ import { Switch, Route, Link, Redirect } from 'react-router-dom'
 import Login from './Login'
 import Dashboard from './Dashboard'
 import { useAuth } from '../services/use-auth'
+import axios from 'axios'
+import FileBase64 from "react-file-base64"
 
 function Gameboard() {
+  const [image64, setImage64] = useState('')
+    
+
+  const onFileChange = (e) => {
+    setImage64()
+  }
+
+  console.log('image64: ', image64)
   return (
-    <div>This is the gameboard!</div>
+    <>
+    <FileBase64
+      multiple={false}
+      onDone={({ base64 }) => setImage64(base64)} />
+    <div className="row apple mapRow">
+      <div className="col-8 mapBox">
+        <img className="img-fluid cake" src={image64} />
+      </div>
+    </div>
+    
+    </>
   )
 }
 
