e69e1cf3d3610d2efbb7cd496dea5ac5cb6623d0
Draper-dev <tdanieldraper@gmail.com>
2021-08-28 15:42:16 -0400

    css tweaks for CreatedBy logo

--- src/components/Login.js
+++ src/components/Login.js
@@ -245,12 +245,13 @@ function Login() {
 
   return (
     <div id="background">
-      <CreatorLogo/>
+    <CreatorLogo/>
     <NotificationSuccess successType='userCreated' />
     <h1 className="noselect loginTitle">
         Dungeon Map!
     </h1>
     <div className='loginContainer row mx-3'>
+    
       <div className='loginBox notifyBoxes d-flex'>
         <h2 className="noselect titles formTitle">Login</h2>
         <div className="formBoxContainer py-5">
@@ -269,7 +270,6 @@ function Login() {
           
       </div>
     </div>
-    
   </div>
   )
 }
